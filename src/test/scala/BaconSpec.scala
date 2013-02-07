cc66bbc3e8f3585e3dab7c9fdeefb364fbd380a4
Juha Paananen <juha.paananen@gmail.com>
2013-02-08 00:10:21 +0200

    simplify. learning scala

--- src/test/scala/BaconSpec.scala
+++ src/test/scala/BaconSpec.scala
@@ -9,51 +9,51 @@ class BaconSpec extends Specification {
 
   "Bacon.once" should {
     "produce one value" in {
-      expectStreamEvents(() => Bacon.once("bacon"), "bacon")
+      expectStreamEvents(Bacon.once("bacon"), "bacon")
     }
   }
   "Bacon.fromValues" should {
     "produce values from list" in {
-      expectStreamEvents(() => Bacon.fromValues(1, 2, 3), 1, 2, 3)
+      expectStreamEvents(Bacon.fromValues(1, 2, 3), 1, 2, 3)
     }
   }
   "Bacon.later" should {
     "produce one value" in {
-      expectStreamEvents(() => Bacon.later(T(1), "hello"), "hello")
+      expectStreamEvents(Bacon.later(T(1), "hello"), "hello")
     }
   }
   "Bacon.sequentially" should {
     "produce a list of values" in {
-      expectStreamEvents(() => series(1, List(1,2,3)), 1,2,3)
+      expectStreamEvents(series(1, List(1,2,3)), 1,2,3)
     }
   }
   "EventStream.map" should {
     "map values using given function" in {
-      expectStreamEvents(() => series(1, List(1,2,3)).map(_ * 10), 10, 20, 30)
+      expectStreamEvents(series(1, List(1,2,3)).map(_ * 10), 10, 20, 30)
     }
   }
   "EventStream.filter" should {
     "filter values using given function" in {
-      expectStreamEvents(() => series(1, List(1,2,3,4,5)).filter(_ % 2 == 0), 2, 4)
+      expectStreamEvents(series(1, List(1,2,3,4,5)).filter(_ % 2 == 0), 2, 4)
     }
   }
   "EventStream.merge" should {
     "merge values from two streams" in {
       expectStreamEvents(
-        () => series(2, List("a", "b")).merge(series(3, List("x", "y"))),
+        series(2, List("a", "b")).merge(series(3, List("x", "y"))),
         "a", "x", "b", "y")
     }
   }
   "EventStream.flatMap" should {
     "spawn new streams and collect results" in {
       expectStreamEvents(
-        () => series(1, List(1,2)).flatMap {
+        series(1, List(1,2)).flatMap {
           value => series(2, List(value, value))
         },
         1, 2, 1, 2
       )
       expectStreamEvents(
-        () => series(3, List(1,2)).flatMap {
+        series(3, List(1,2)).flatMap {
           value => series(1, List(value, value))
         },
         1, 1, 2, 2
@@ -63,9 +63,9 @@ class BaconSpec extends Specification {
   "EventStream.delay" should {
     "delay each event by given number of millis" in {
       expectStreamEvents(
-        () => { val left = series(2, List(1, 2, 3))
-                val right = series(1, List(4, 5, 6)).delay(T(6))
-                left.merge(right)
+        { val left = series(2, List(1, 2, 3))
+          val right = series(1, List(4, 5, 6)).delay(T(6))
+          left.merge(right)
         },
         1, 2, 3, 4, 5, 6)
     }
@@ -81,21 +81,21 @@ class BaconSpec extends Specification {
   }
   var seqs: List[EventStream[_]] = Nil
 
-  def expectStreamEvents[T](src: () => EventStream[T], expectedValues: T*) = {
+  def expectStreamEvents[T](src: => EventStream[T], expectedValues: T*) = {
     verifySingleObserver(src, expectedValues : _*)
     verifySwitching(src, expectedValues : _*)
   }
 
-  def verifySingleObserver[T](src: () => EventStream[T], expectedValues: T*) = {
-    val stream = src()
+  def verifySingleObserver[T](src: => EventStream[T], expectedValues: T*) = {
+    val stream = src
     val values = drain(stream)
     values must_== expectedValues
     verifyExhausted(stream)
     verifyCleanup
   }
 
-  def verifySwitching[T](src: () => EventStream[T], expectedValues: T*) = {
-    val stream = src()
+  def verifySwitching[T](src: => EventStream[T], expectedValues: T*) = {
+    val stream = src
     var vs: List[T] = Nil
     var result = new MVar[List[T]]
     def newObserver: Observer[T] = {
